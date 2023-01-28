c6c11f7538d2c5b2c19430f0e3f5f81058181f25
eungwang <scm323@naver.com>
2023-01-28 17:11:57 +0900

    Feat: birthdat_screen layout제작

---
+++ lib/features/authentication/birthday_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/interests_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+
+class BirthdayScreen extends StatefulWidget {
+  const BirthdayScreen({super.key});
+
+  @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+  final DateTime now = DateTime.now();
+  late final DateTime initialDate;
+
+  @override
+  void initState() {
+    super.initState();
+    initialDate = DateTime(now.year - 1, 12, 31);
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const InterestsScreen(),
+    ));
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Sign up",
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text(
+              "When's your birthday?",
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text(
+              "Your birthdat won't be shown publicly",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+            ),
+            Gaps.v16,
+            GestureDetector(
+              onTap: () => _onNextTap(context),
+              child: const FormButton(disabled: false),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+              height: 300,
+              child: CupertinoDatePicker(
+                maximumDate: initialDate,
+                initialDateTime: initialDate,
+                mode: CupertinoDatePickerMode.date,
+                onDateTimeChanged: _setTextFieldDate,
+              ))),
+    );
+  }
+}
